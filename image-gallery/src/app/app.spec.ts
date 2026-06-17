9c8139cca66cfcab7c39ea2c3238cc310c8263f6
jessica <rodriguezjessika36@gmail.com>
2026-06-17 22:02:41 +0200

    test fix

--- image-gallery/src/app/app.spec.ts
+++ image-gallery/src/app/app.spec.ts
@@ -18,6 +18,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, image-gallery');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Mi Galería de Imágenes');
   });
 });
