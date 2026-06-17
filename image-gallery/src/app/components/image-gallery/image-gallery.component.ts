543df4325944eec6e6b9143dc86253776ee828ac
jessica <rodriguezjessika36@gmail.com>
2026-06-17 21:51:16 +0200

    add gallery for image item

---
+++ image-gallery/src/app/components/image-gallery/image-gallery.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, signal } from '@angular/core';
+import { Image, IMAGES } from '../../interfaces/image.interface';
+import { ImageItemComponent } from '../image-item/image-item.component';
+
+@Component({
+  selector: 'app-image-gallery',
+  standalone: true,
+  imports: [ImageItemComponent],
+  templateUrl: './image-gallery.component.html',
+  styleUrls: ['./image-gallery.component.css']
+})
+export class ImageGalleryComponent implements OnInit {
+  images = signal<Image[]>([]);
+
+  ngOnInit() {
+    this.images.set(IMAGES.filter(img => !img.borrado));
+  }
+}
+
+
