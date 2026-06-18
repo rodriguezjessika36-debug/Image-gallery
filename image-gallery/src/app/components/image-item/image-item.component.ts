f851e7a9bd25a6e2e6f7852edfc240865e3161b1
jessica <rodriguezjessika36@gmail.com>
2026-06-18 13:17:18 +0200

    Primeng use for design

--- image-gallery/src/app/components/image-item/image-item.component.ts
+++ image-gallery/src/app/components/image-item/image-item.component.ts
@@ -1,9 +1,13 @@
 import { Component, input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { Image } from '../../interfaces/image.interface';
+import { CardModule } from 'primeng/card';
+import { TagModule } from 'primeng/tag';
 
 @Component({
   selector: 'app-image-item',
   standalone: true,
+  imports: [CommonModule, CardModule, TagModule],
   templateUrl: './image-item.component.html',
   styleUrls: ['./image-item.component.css']
 })
