81f8e56143fb71da885fbeb215aee8f11b7af02f
jessica <rodriguezjessika36@gmail.com>
2026-06-19 00:37:14 +0200

    Drag and drop logic

--- image-gallery/src/app/components/image-gallery/image-gallery.component.ts
+++ image-gallery/src/app/components/image-gallery/image-gallery.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit, signal } from '@angular/core';
 import { Image, IMAGES } from '../../interfaces/image.interface';
 import { ImageItemComponent } from '../image-item/image-item.component';
+import { CdkDropList, CdkDrag, CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-image-gallery',
   standalone: true,
-  imports: [ImageItemComponent],
+  imports: [ImageItemComponent, CdkDropList, CdkDrag],
   templateUrl: './image-gallery.component.html',
   styleUrls: ['./image-gallery.component.css']
 })
@@ -17,9 +18,17 @@ export class ImageGalleryComponent implements OnInit {
   }
 
   onDeleteImage(imageId: number): void {
-    this.images.update(currentImages => 
+    this.images.update(currentImages =>
       currentImages.filter(img => img.id !== imageId)
     );
+  }
+
+  onDrop(event: CdkDragDrop<Image[]>): void {
+    this.images.update(current => {
+      const reordered = [...current];
+      moveItemInArray(reordered, event.previousIndex, event.currentIndex);
+      return reordered;
+    });
   }
 }
 
